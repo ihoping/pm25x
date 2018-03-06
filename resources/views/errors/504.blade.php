bac48f22f7016eedce37695d87bdd2b78ec4262f
lutiansong <new.gooltsgle@gmail.com>
2018-03-06 10:31:18 +0800

    add 504 error view

---
+++ resources/views/errors/504.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'whoops！ 504错误')
+@section('content')
+    <div class="error-message" style="text-align: center;margin-top: 10%">
+        <h2><?php echo $exception->getMessage() ?></h2>
+    </div>
+@endsection
