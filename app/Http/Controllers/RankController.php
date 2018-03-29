2da7a784e13b47cec53008c8628650edfcc9f810
lutiansong <new.gooltsgle@gmail.com>
2018-03-29 13:47:51 +0800

    review

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -15,10 +15,6 @@ class RankController extends Controller
       */
     private $ip = [];
     /*
-     * 全部地区数组
-     */
-    private $areas = [];
-    /*
      * 用户所在地区
      * String
      */
@@ -26,32 +22,11 @@ class RankController extends Controller
 
     private $rank_types = ['now', 'yesterday', '7day', 'last_month'];
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $areas_obj = Areas::all();
-        foreach ($areas_obj as $row) {
-            $this->areas[] = $row->name;
-        }
-
-    }
 
     //
     public function index(Request $request)
     {
-        if (Cookie::has('area')) {
-            $this->my_area = Cookie::get('area');
-            //dd($this->my_area);
-        } else {
-            $request->setTrustedProxies(array('10.32.0.1/16'));
-            $this->ip = $request->getClientIp();
-            $this->my_area = getAddr($this->ip);
-            Cookie::queue('area', $this->my_area, 24 * 60 * 10);
-        }
+        $this->setArea($request);
 
         $type = $this->checkType($request->route('type'));
         $rank_data = $this->getRankData($type);
@@ -122,4 +97,31 @@ class RankController extends Controller
         }
         return $data;
     }
+
+    /*
+   * 设置地区
+   */
+    private function setArea(Request $request)
+    {
+        if (Cookie::has('area')) {//如果cookie里有地区信息了
+            $this->my_area = Cookie::get('area');
+            //dd($this->my_area);
+        } else {//没有再去获取
+            $request->setTrustedProxies(array('10.32.0.1/16'));
+            $this->ip = $request->getClientIp();
+            $this->my_area = getAddr($this->ip);
+            $areas_obj = Areas::all();
+            foreach ($areas_obj as $row) {
+                $areas[] = $row->name;
+            }
+            foreach ($areas as $area) {
+                if (mb_strrpos($area, $this->my_area) !== false) {
+                    Cookie::queue('area', $this->my_area, 24 * 60 * 10);
+                } else {
+                    Cookie::queue('area', '南京', 24 * 60 * 10);
+                }
+            }
+        }
+    }
+
 }
--- app/Http/Controllers/VisualController.php
+++ app/Http/Controllers/VisualController.php
@@ -13,45 +13,45 @@ class VisualController extends Controller
       */
     private $ip = [];
     /*
-     * 全部地区数组
-     */
-    private $areas = [];
-    /*
      * 用户所在地区
      * String
      */
     private $my_area = '';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $areas_obj = Areas::all();
-        foreach ($areas_obj as $row) {
-            $this->areas[] = $row->name;
-        }
-
-    }
     //
     public function index(Request $request)
     {
-        if (Cookie::has('area')) {
-            $this->my_area = Cookie::get('area');
-            //dd($this->my_area);
-        } else {
-            $request->setTrustedProxies(array('10.32.0.1/16'));
-            $this->ip = $request->getClientIp();
-            $this->my_area = getAddr($this->ip);
-            Cookie::queue('area', $this->my_area, 24 * 60 * 10);
-        }
+        $this->setArea($request);
 
         $data = [
             'nav' => 'visual',
             'area' => $this->my_area
         ];
         return view('main.visual', $data);
+    }
+    /*
+   * 设置地区
+   */
+    private function setArea(Request $request)
+    {
+        if (Cookie::has('area')) {//如果cookie里有地区信息了
+            $this->my_area = Cookie::get('area');
+            //dd($this->my_area);
+        } else {//没有再去获取
+            $request->setTrustedProxies(array('10.32.0.1/16'));
+            $this->ip = $request->getClientIp();
+            $this->my_area = getAddr($this->ip);
+            $areas_obj = Areas::all();
+            foreach ($areas_obj as $row) {
+                $areas[] = $row->name;
+            }
+            foreach ($areas as $area) {
+                if (mb_strrpos($area, $this->my_area) !== false) {
+                    Cookie::queue('area', $this->my_area, 24 * 60 * 10);
+                } else {
+                    Cookie::queue('area', '南京', 24 * 60 * 10);
+                }
+            }
+        }
     }
 }
